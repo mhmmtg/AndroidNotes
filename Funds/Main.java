f9081b749063819e8a9242943cd7d523b9b8c155
mhmmtg <37017916+mhmmtg@users.noreply.github.com>
2021-11-05 13:26:41 +0300

    Delete Main.java

--- Funds/Main.java
+++
@@ -1,9 +0,0 @@
-package com.mguler.mylogjava;
-
-//main ilk calistirilan kodlar
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Merhaba Java");
-        System.out.println(5*2);
-    }
-}
